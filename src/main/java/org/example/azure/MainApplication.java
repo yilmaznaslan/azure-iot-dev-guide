ee1a2065f94aa11676c4921b85b42d384a66e22f
Yilmaz Naci Aslan <yilmaznaci.aslan@maibornwolff.de>
2022-10-06 08:40:01 +0200

    add getter

--- src/main/java/org/example/azure/MainApplication.java
+++ src/main/java/org/example/azure/MainApplication.java
@@ -13,15 +13,26 @@ public class MainApplication {
 
     public static void main(String[] args) throws Exception {
         patchDeviceTwin("dishwasher_2");
+        getTwinTags("dishwasher_2");
     }
     public static void patchDeviceTwin(String deviceId) throws IOException, IotHubException {
         TwinClient twinClient = new TwinClient(iotHubConnectionString);
         Twin twin = twinClient.get(deviceId);
         TwinCollection twinCollection = new TwinCollection();
         twinCollection.put("customerId", "null");
-        twinCollection.put("country", "germany");
+        twinCollection.put("country", "usa");
         twinCollection.put("SoftwareId", "xe123");
         //twin.setTags(twinCollection);
         twinClient.patch(twin);
+    }
+    public static void getTwinTags(String deviceId) throws IOException, IotHubException {
+        TwinClient twinClient = new TwinClient(iotHubConnectionString);
+        Twin twin = twinClient.get(deviceId);
+        TwinCollection twinCollection = twin.getTags();
+        String countryValue  = twinCollection.get("country").toString();
+        System.out.println("Country value of the twin is:" + countryValue);
+        if(!countryValue.equals("usa")){
+            System.out.println("Updating country tag has failed");
+        }
     }
 }
